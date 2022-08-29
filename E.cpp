383a08f4b4fb660199a22fe900c3fac8b2a343ed
AZMSharif <110534962+AZMSharif@users.noreply.github.com>
2022-08-29 22:53:25 +0600

    Create E.cpp

---
+++ E.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    double r,p,x;
+    cin>>r;
+    p=3.141592653;
+    x=(r*r)*p;
+    cout<<setprecision(11)<<x<<endl;
+    return 0;
+}
