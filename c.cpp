e1874564d89696b4d3506ff91447abb9eef24e13
AZMSharif <110534962+AZMSharif@users.noreply.github.com>
2022-08-29 21:50:34 +0600

    Create c.cpp

---
+++ c.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    long long int x,y;
+    long long int p,q,r;
+    cin>>x>>y;
+    p = x+y;
+    q = x*y;
+    r = x-y;
+    cout<<x<<" + "<<y<<" = "<<p<<endl;
+    cout<<x<<" * "<<y<<" = "<<q<<endl;
+    cout<<x<<" - "<<y<<" = "<<r<<endl;
+    return 0;
+}
