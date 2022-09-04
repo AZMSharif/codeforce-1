471888e3356fd3c0a7293b2e9ae58a688eea914b
AZMSharif <110534962+AZMSharif@users.noreply.github.com>
2022-09-04 13:52:54 +0600

    Create P.cpp

---
+++ P.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,p;
+    cin>>x;
+    p=x/1000;
+    if(p%2==0)
+    {
+        cout<<"EVEN"<<endl;
+    }
+    else{
+        cout<<"ODD"<<endl;
+    }
+    return 0;
+}
