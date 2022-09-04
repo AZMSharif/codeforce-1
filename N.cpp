e8d1c36baa5dc3bf4aa33e168cbc4eb5113b93a2
AZMSharif <110534962+AZMSharif@users.noreply.github.com>
2022-09-04 13:31:44 +0600

    Create N.cpp

---
+++ N.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char x;
+    cin>>x;
+    if(x>=65&&x<=90)
+    {
+        cout<<char(x+32)<<endl;
+    }
+    else if(x>=97&&x<=122)
+    {
+        cout<<char(x-32)<<endl;
+    }
+    return 0;
+}
